31d14d4be48e012c191f8aeb9e445dc1e982e1a4
Velgrym <savalenciaan44@gmail.com>
2023-03-16 00:59:08 -0500

    Changing the database

--- prisma/migrations/20230316054702_init/migration.sql
+++ prisma/migrations/20230316054702_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Answer" (
     "name" TEXT NOT NULL,
     "age" TEXT NOT NULL,
-    "contact" TEXT NOT NULL PRIMARY KEY,
+    "contact" TEXT NOT NULL,
     "color" TEXT NOT NULL,
     "institution" TEXT NOT NULL,
     "challenges" TEXT NOT NULL,
@@ -17,5 +17,16 @@ CREATE TABLE "Answer" (
     "logo" TEXT NOT NULL,
     "value" TEXT NOT NULL,
     "rate" INTEGER NOT NULL,
-    "innovation" TEXT NOT NULL
+    "innovation" TEXT NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("contact")
+);
+
+-- CreateTable
+CREATE TABLE "contacts" (
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("email")
 );
